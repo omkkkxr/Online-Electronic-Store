e825c740abef56eea98b6101572195ebee01e5d2
Omkar Karpe <125791646+omkkkxr@users.noreply.github.com>
2024-11-18 23:17:00 +0530

    Adding database files

---
+++ admin.sql
@@ -0,0 +1,16 @@
+SQL> [A[Bspool admin.sql
+SP2-0734: unknown command beginning "[A[Bspoo..." - rest of line ignored.
+SQL> select*from admin;
+
+UNAME                     PWORD                     FNAME                       
+------------------------- ------------------------- -------------------------   
+LNAME                     ADDR                      MAIL                        
+------------------------- ------------------------- -------------------------   
+PHNO                                                                            
+-------------------------                                                       
+admin                     123                       Omkar                       
+Karpe                     Sai residency,Sus         omkarkarpe123@gmail.com     
+7843005956                                                                      
+                                                                                
+
+SQL> spool off;
